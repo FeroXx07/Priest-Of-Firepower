a6533e6659dfa6b91222ee7834c0e51ce1fe8b48
Allen Bateman <57528826+allenbateman@users.noreply.github.com>
2023-10-06 12:40:25 +0200

    A update

--- Priest of Firepower/Assets/_Scripts/Networking/A/AClient.cs
+++ Priest of Firepower/Assets/_Scripts/Networking/A/AClient.cs
@@ -1,18 +1,88 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Security;
 using UnityEngine;
+using System.Threading;
+using System.Text;
 
 public class AClient : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Socket socket;
+    [SerializeField] AddressFamily addressFamily;
+    [SerializeField] SocketType socketType;
+    [SerializeField] ProtocolType protocolType;
+
+    IPEndPoint endPoint;
+    [SerializeField] IPAddress IPaddress;
+    [SerializeField] int port;
+
+    Thread sendData;
+    private void OnEnable()
     {
-        
+
+        sendData = new Thread(() => SendUDP());
+
+        sendData.Start();
+
+    }
+    private void OnDisable()
+    {
+        socket.Close();
+        sendData.Abort();   
     }
 
-    // Update is called once per frame
-    void Update()
+    void SendUDP()
     {
+        if (socket == null)
+        {
+            socket = new Socket(addressFamily, socketType, protocolType);
+            endPoint = new IPEndPoint(IPAddress.Any, port);
+        }
+        Byte[] sendBytes = Encoding.ASCII.GetBytes("Hello there! from clientA");
+        socket.SendTo(sendBytes,sendBytes.Length,SocketFlags.None, endPoint);
+
+
+    }
+
+    void SendTCP()
+    {
+
+    }
+
+    void Bind(Socket socketToBind, IPEndPoint endpointToBind)
+    {
+        try
+        {
+            socketToBind.Bind(endpointToBind);
+
+        }catch(SocketException e)
+        {
+            Debug.LogException(e);
+
+        }catch(ArgumentNullException e)
+        {
+            Debug.LogException(e);
+
+        }catch(ObjectDisposedException e) 
+        { 
+            Debug.LogException(e);
         
+        }catch(SecurityException e)
+        {
+            Debug.LogException(e);
+        }
+       
+    }
+
+    void CreateSocket(AddressFamily family, SocketType type, ProtocolType protocol)
+    {
+        socket = new Socket(family, type, protocol);
+    }
+    void CreateEndPoint(long ipAddress, int port)
+    {
+        endPoint = new IPEndPoint(ipAddress, port); 
     }
 }
--- Priest of Firepower/Assets/_Scripts/Networking/A/AServer.cs
+++ Priest of Firepower/Assets/_Scripts/Networking/A/AServer.cs
@@ -1,18 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
 using UnityEngine;
+using System.Security;
+using System;
+using Unity.VisualScripting;
+using System.Threading;
 
 public class AServer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Socket socket;
+    [SerializeField] AddressFamily addressFamily;
+    [SerializeField] SocketType socketType;
+    [SerializeField] ProtocolType protocolType;
+
+    IPEndPoint endPoint;
+    [SerializeField] IPAddress IPaddress;
+    [SerializeField] int port;
+
+    Thread listen;
+    private void OnEnable()
     {
-        
+        listen = new Thread(()=> InitServer()); 
+    }
+    void InitServer()
+    {
+        socket = new Socket(addressFamily, socketType, protocolType);
+        Bind(socket, endPoint);
+        while (true)
+        {
+            try
+            {
+                socket.Listen(1);
+                Debug.Log("waiting for clients....");
+                Socket client = socket.Accept();
+                IPEndPoint clientIP = (IPEndPoint)client.RemoteEndPoint;
+
+
+            }catch(SocketException e)
+            {
+                Debug.LogException(e);
+            }
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void Bind(Socket socketToBind, IPEndPoint endpointToBind)
     {
-        
+        try
+        {
+            socketToBind.Bind(endpointToBind);
+
+        }
+        catch (SocketException e)
+        {
+            Debug.LogException(e);
+
+        }
+        catch (ArgumentNullException e)
+        {
+            Debug.LogException(e);
+
+        }
+        catch (ObjectDisposedException e)
+        {
+            Debug.LogException(e);
+
+        }
+        catch (SecurityException e)
+        {
+            Debug.LogException(e);
+        }
+
     }
+
 }
