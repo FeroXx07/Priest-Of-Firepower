2f8dc9e399a1b2bb3699765795eaf84373c29c3d
IconicGIT <72080813+IconicGIT@users.noreply.github.com>
2023-10-06 12:44:49 +0200

    Separated B into Alexandre and Brandon

---
+++ Priest of Firepower/Assets/Scenes/Nettest/Brandon/Server/UDP_Server.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System;
+using System.Net.Sockets;
+
+public class UDP_Server : MonoBehaviour
+{
+    private UdpClient udpServer;
+    public int port = 5000;
+    string serverIpAddress = " 192.168.104.17";
+    bool createRoomRequested = false;
+    Socket newSocket;
+    EndPoint RemoteClient;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    void CreateRoomRequest()
+    {
+        createRoomRequested = true;
+        newSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        
+    }
+
+    void ListenForClients()
+    {
+        var client = new IPEndPoint(IPAddress.Any, port);
+        RemoteClient
+        newSocket.Bind(client);
+    }
+
+    void RecieveCallback(IAsyncResult ar)
+    {
+        byte[] msg = new Byte[256];
+        Console.WriteLine("Waiting to receive datagrams from client...");
+        // This call blocks.
+        newSocket.ReceiveFrom(msg, msg.Length, SocketFlags.None ref client);
+        newSocket.Close();
+    }
+
+    
+}
