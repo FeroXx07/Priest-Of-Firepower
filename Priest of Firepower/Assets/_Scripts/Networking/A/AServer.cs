ebb98f92132c80a79a449e5b6970512ed5319c7d
Allen Bateman <57528826+allenbateman@users.noreply.github.com>
2023-10-06 11:40:11 +0200

    A B competition start

--- Priest of Firepower/Assets/_Scripts/Networking/A/AServer.cs
+++ Priest of Firepower/Assets/_Scripts/Networking/A/AServer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Server : MonoBehaviour
+public class AServer : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
