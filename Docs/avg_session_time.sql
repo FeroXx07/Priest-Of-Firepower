30003a469d52740ea3d9db10458e3bdc93f218ea
IconicGIT <72080813+IconicGIT@users.noreply.github.com>
2023-10-26 14:26:06 +0200

    ups eso era para datos

--- Docs/avg_session_time.sql
+++
@@ -1,5 +0,0 @@
-#CALCULATE AVERAGE SESSION TIME
-SELECT AVG(TIMESTAMPDIFF(SECOND, Sessions.loginStamp, Sessions.logoutStamp))/60 
-AS "Average playTime" 
-FROM Sessions, Users 
-WHERE Sessions.userId = Users.id
--- Docs/players_per_country.sql
+++
@@ -1,4 +0,0 @@
-#CALCULATE NUMBER OF PLAYERS PER COUNTRY
-SELECT Users.country AS "Country", count(Users.id) AS "User Per Country"
-FROM  Users 
-GROUP BY Users.Country 
