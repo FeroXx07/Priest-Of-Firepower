865c94f3f5e604b38ae3fae1bd0a1987c2f6bc28
allenbateman <57528826+allenbateman@users.noreply.github.com>
2023-11-23 16:08:43 +0100

    Connect to lobby update

--- Priest of Firepower/Assets/_Scripts/Networking/NetworkManagerHUD.cs
+++ Priest of Firepower/Assets/_Scripts/Networking/NetworkManagerHUD.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -10,16 +11,23 @@ namespace _Scripts.Networking
     public class NetworkManagerHUD : MonoBehaviour
     {
         [SerializeField] private string nextSceneToLoad = "Game_Networking_Test";
-        [SerializeField] Button hostBtn;
-        [SerializeField] TMP_InputField ipInputField;
-        [SerializeField] TMP_InputField userNameInputField;
-        
+        [SerializeField] private Button hostBtn;
+        [Header("Join Game")]
+        [SerializeField] private TMP_InputField ipInputField;
+        [SerializeField] private TMP_Text ipTextfield;
+        [Header("Username")]
+        [SerializeField] private TMP_InputField userNameInputField;
+        [SerializeField] private TMP_Text priestName;
 
+
+        private bool isNameSet = false;
         private void OnEnable()
         {
             hostBtn.onClick.AddListener(HostGame);
             NetworkManager.Instance.OnClientConnected += Lobby;
             ipInputField.onEndEdit.AddListener(ConnectToServer);
+            userNameInputField.onEndEdit.AddListener(SetPriestName);
+            isNameSet = false;
         }
 
         private void OnDisable()
@@ -27,10 +35,17 @@ namespace _Scripts.Networking
             hostBtn.onClick.RemoveListener(HostGame);
             NetworkManager.Instance.OnClientConnected -= Lobby;
             ipInputField.onEndEdit.RemoveListener(ConnectToServer);
+            userNameInputField.onEndEdit.RemoveListener(SetPriestName);
         }
         
         void HostGame()
         {
+            if (!isNameSet)
+            {
+                priestName.color = Color.red;
+                priestName.text = "Name can't be empty";
+                return;
+            }
             NetworkManager.Instance.StartHost();
         }
 
@@ -43,6 +58,14 @@ namespace _Scripts.Networking
 
         private void ConnectToServer(string ip)
         {
+
+            if (!isNameSet)
+            {
+                priestName.color = Color.red;
+                priestName.text = "Name can't be empty";
+                return;
+            }
+            
             NetworkManager manager = NetworkManager.Instance;
 
             if (IPAddress.TryParse(ip, out manager.serverAdress))
@@ -55,8 +78,44 @@ namespace _Scripts.Networking
             }
             else
             {
+                ipInputField.text = "Invalid IP address";
+                ipTextfield.color = Color.red;
                 Debug.LogError(ip + " address is not valid ...");
             }
+        }
+
+        void SetPriestName(string name)
+        {
+            if (ValidateName(name))
+            {
+                NetworkManager.Instance.PlayerName = name;
+                isNameSet = true;
+                priestName.text = name;
+                priestName.color = Color.white;
+            }
+        }
+
+        bool ValidateName(string nameToValidate)
+        {
+            bool validation = true;
+
+            if (nameToValidate == "")
+            {
+                validation = false;
+                isNameSet = false;
+                priestName.color = Color.red;
+                priestName.text = "Name can't be empty";
+            }
+
+            if (nameToValidate.Length > 10)
+            {
+                validation = false;
+                isNameSet = false;
+                priestName.color = Color.red;
+                priestName.text = "Name too long";
+            }
+        
+            return validation;
         }
     }
 }
