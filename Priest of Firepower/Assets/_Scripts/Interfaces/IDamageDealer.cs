094b4405b066c92da0f29afcdca9d35f695793ce
FeroXx07 <59049844+FeroXx07@users.noreply.github.com>
2023-12-24 19:56:23 +0100

    Update IDamageDealer.cs

--- Priest of Firepower/Assets/_Scripts/Interfaces/IDamageDealer.cs
+++ Priest of Firepower/Assets/_Scripts/Interfaces/IDamageDealer.cs
@@ -6,7 +6,7 @@ namespace _Scripts.Interfaces
     public interface IDamageDealer
     {
         event Action<GameObject> OnDamageDealerDestroyed;
-        public event Action<GameObject> OnDamageDealth;
+        event Action<GameObject> OnDamageDealth;
         int Damage { get; set; }
         void ProcessHit(IDamageable damageable, Vector3 dir, GameObject hitOwnerGameObject, GameObject hitterGameObject,
             GameObject hittedGameObject);
