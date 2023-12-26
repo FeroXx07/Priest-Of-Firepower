95a3113938c4c84c3c9513e8887e067f8e23d2a2
FeroXx07 <59049844+FeroXx07@users.noreply.github.com>
2023-12-26 05:15:28 +0100

    Serialized Dictionaries
    Very useful dictionaries that can be exposed in the inspector

---
+++ Priest of Firepower/Assets/UnitySerializedDictionary/Examples/ScriptHolder.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Collections;
+using UnityEngine;
+using UnityEngine.Collections.Generic;
+
+public class ScriptHolder : MonoBehaviour
+{
+    [SerializeField] private UDictionaryIntColor intColor;
+
+    [SerializeField]
+    [ReadOnly]
+    private UDictionaryStringInt stringIntReadOnly;
+    [SerializeField] private UDictionaryIntStruct intStruct;
+    [SerializeField] private UDictionaryStructStruct structStruct;
+    [SerializeField] private UDictionaryDoubleClass doubleClass;
+
+    public UDictionaryNoSerializebleKey noSerializebleKey;
+    public UDictionaryNoSerializebleValue noSerializebleValue;
+
+    private void Start()
+    {
+        stringIntReadOnly.Add("a", 1);
+        stringIntReadOnly.Add("b", 1);
+        stringIntReadOnly.Add("c", 1);
+    }
+}
---
+++ Priest of Firepower/Assets/UnitySerializedDictionary/Examples/UDictionaryExamples.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Collections.Generic;
+
+[Serializable]
+public class UDictionaryStringInt : UDictionary<string, int> { }
+
+[Serializable]
+public class UDictionaryIntStruct : UDictionary<int, OtherStruct> { }
+
+[Serializable]
+public class UDictionaryStructStruct : UDictionary<Struct, OtherStruct> { }
+
+[Serializable]
+public class UDictionaryDoubleClass : UDictionary<double, Class> { }
+
+[Serializable]
+public class UDictionaryNoSerializebleKey : UDictionary<NoSerializebleClass, int> { }
+
+[Serializable]
+public class UDictionaryNoSerializebleValue : UDictionary<int, NoSerializebleClass> { }
+
+[Serializable]
+public struct Struct
+{
+    public string Text;
+    public int Int;
+}
+
+[Serializable]
+public struct OtherStruct
+{
+    public string Text;
+    public float Float;
+    public int Int;
+    public Color Color;
+}
+
+[Serializable]
+public class Class
+{
+    public string Text;
+    public int Int;
+}
+
+public class NoSerializebleClass
+{
+    public int Int;
+    public string Text;
+}
